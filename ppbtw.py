3519a8e1e7f4803702288dbd3cf5ee0c81cfe776
Bjoern <82806099+Soulprayer79@users.noreply.github.com>
2023-11-20 13:02:34 +0100

    Update ppbtw.py
    Aktualisierung des Skripts

--- ppbtw.py
+++ ppbtw.py
@@ -1,46 +1,26 @@
 import datetime
-import calendar
 import locale
 from datetime import date
 from datetime import timedelta
-locale.setlocale(locale.LC_ALL, '')
 
-days = range(7) # 1 bis 7
-addDays = timedelta(days=1)
-addWeek = timedelta(days=7)
+# Jahr festlegen
+jahreingabe = input('Eingabe des Jahres: ')
+jahr = date(int(jahreingabe), 1, 1)
 
-tTable = '{|class="perrypedia_std_table" style="background:#CDCDC1;" '
-tHeader = "! width=12,5% |<center>Montag</center>\n"
-tHeader = tHeader + "! width=12,5% |<center>Dienstag</center>\n"
-tHeader = tHeader + "! width=12,5% |<center>Mittwoch</center>\n"
-tHeader = tHeader + "! width=12,5% |<center>Donnerstag</center>\n"
-tHeader = tHeader + "! width=12,5% |<center>Freitag</center>\n"
-tHeader = tHeader + "! width=12,5% |<center>Samstag</center>\n"
-tHeader = tHeader + "! width=12,5% |<center>Sonntag</center>\n"
+tTable = '{|class="perrypedia_std_table" style="background:#CDCDC1;\n' 
+tTable = tTable + "! width=16% |<center>Fakt/Zitat</center>\n"
+tTable = tTable + "! width=12% |<center>Montag</center>\n"
+tTable = tTable + "! width=12% |<center>Dienstag</center>\n"
+tTable = tTable + "! width=12% |<center>Mittwoch</center>\n"
+tTable = tTable + "! width=12% |<center>Donnerstag</center>\n"
+tTable = tTable + "! width=12% |<center>Freitag</center>\n"
+tTable = tTable + "! width=12% |<center>Samstag</center>\n"
+tTable = tTable + "! width=12% |<center>Sonntag</center>"
 
 tVorlageBegin= "|----"
 tVorlageZF = "|<center>[[Vorlage:Hauptseite_Zitate_KW{0:02}_{1}|Zitat-{0:02}]]&nbsp;/&nbsp;[[Vorlage:Hauptseite_Fakten_KW{0:02}_{1}|Fakt-{0:02}]]</center>"
 tVorlageTag = "|<center>[[Vorlage:Hauptseite_BTW_{0:02}_{1:02}_{2}|{0}.&nbsp;{3:%B}]]</center>"
 tVorlageEnde = "|}"
-
-date_year = input('Eingabe des Jahres: ')
-schaltjahr = calendar.isleap(int(date_year))
-
-dt_checkKW = date(int(date_year), 1, 1)
-if dt_checkKW.weekday() == 3:
-	weeks = range(1, 54) #1 bis 53
-	beginfd = 1
-else:
-	weeks = range(1, 53) #1 bis 52
-	beginfd = 0
-	
-def writeheader(date):
-	myyear=int(date.year)
-	myyear=(2000-myyear)*-1
-	print (tHeader.format(myyear))
-
-def first_day_of_week(date):
-	return date + datetime.timedelta(days = -date.weekday())
 
 def print_day(date):
 	myday=int(date.day)
@@ -48,20 +28,29 @@ def print_day(date):
 	myyear=int(date.year)
 	print (tVorlageTag.format(myday,mymonth,myyear,date))
 
-if beginfd == 0:
-	datumBegin=first_day_of_week(dt_checkKW)+addWeek
-if beginfd == 1:
-	datumBegin=first_day_of_week(dt_checkKW)
-	
+# Erstes Datum des Jahres abrufen
+erstes_datum = datetime.date(int(jahreingabe), 1, 1)
 print (tTable)
-writeheader(dt_checkKW)
-i=1
-j=0
-for i in weeks:
-    print (tVorlageBegin)
-    print (tVorlageZF.format(i,date_year))
-    for j in days:
-        print_day(datumBegin+timedelta(days=j))
-    datumBegin=first_day_of_week(datumBegin+addWeek)
 
+# Schleife über die Wochen des Jahres
+woche = 1
+while True:
+    # Startdatum und Enddatum der aktuellen Woche berechnen
+    start_datum = erstes_datum + datetime.timedelta(weeks=woche-1)
+    end_datum = erstes_datum + datetime.timedelta(weeks=woche) - datetime.timedelta(days=1)
+    
+    # Überprüfen, ob das Enddatum außerhalb des aktuellen Jahres liegt (KW53 abfangen)
+    if end_datum.year > start_datum.year:
+        break
+    print(tVorlageBegin)
+    print (tVorlageZF.format(woche,jahreingabe))
+	
+    for tag in range((end_datum - start_datum).days + 1):
+        # Datum für den aktuellen Tag berechnen
+        datum = start_datum + datetime.timedelta(days=tag)
+	# Ausgabe des aktuellen Tages mit Perry-Format
+        print_day(datum)
+        
+    woche += 1
+    
 print (tVorlageEnde)
